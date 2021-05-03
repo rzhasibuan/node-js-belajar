121221b258e234004920c7198786ad1a0f079d9f
rzhasibuan <rzhasibuan@gmail.com>
2021-05-03 22:03:59 +0700

    handling request

--- server.js
+++ server.js
@@ -1,1 +1,30 @@
-console.log("run this code now");
+const http = require("http");
+
+const requestListener = (request, response) => {
+  const { method } = request;
+
+  if (method === "GET") {
+    response.end("<h1>Hello ini data dari hasil GET</h1>");
+  }
+
+  if (method === "POST") {
+    response.end("<h1>Haii ini data dari hasil POST</h1>");
+  }
+
+  if (method === "PUT") {
+    response.end("<h1>ini dari dari hasil PUT</h1>");
+  }
+
+  if (method === "DELETE") {
+    response.end("<h1>ini data dari hasil DELETE</h1>");
+  }
+};
+
+const server = http.createServer(requestListener);
+
+const port = 5000;
+const host = "localhost";
+
+server.listen(port, host, () => {
+  console.log(`server berjalan pada http://${host}:${port}`);
+});
