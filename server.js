c96f584982504a7f712e3a7db7b1ab3c5c828aa7
rzhasibuan <rzhasibuan@gmail.com>
2021-05-04 14:23:01 +0700

    routing request

--- server.js
+++ server.js
@@ -4,20 +4,49 @@ const requestListener = (request, response) => {
   response.setHeader("Content-type", "text/html");
   response.statusCode = 200;
 
-  const { method } = request;
+  const { method, url } = request;
 
-  if (method === "POST") {
-    let body = [];
+  // if (method === "POST") {
+  //   let body = [];
 
-    request.on("data", (chunk) => {
-      body.push(chunk);
-    });
+  //   request.on("data", (chunk) => {
+  //     body.push(chunk);
+  //   });
 
-    request.on("end", () => {
-      body = Buffer.concat(body).toString();
-      const { name } = JSON.parse(body);
-      response.end(`<h1>Hai, ${name}!</h2>`);
-    });
+  //   request.on("end", () => {
+  //     body = Buffer.concat(body).toString();
+  //     const { name } = JSON.parse(body);
+  //     response.end(`<h1>Hai, ${name}!</h2>`);
+  //   });
+  // }
+
+  if(url === '/'){
+    if(method === 'GET'){
+      response.end('<h1>ini adalah homepage!!</h1>');
+    }else{
+      response.end(`<h1>Halaman tidak dapat di akses dengan ${method} request</h1>`);
+    }
+  }else if(url === '/about'){
+    if(method === 'GET'){
+      response.end('<h1>ini adalah halama about</h1>');
+    }else if(method === 'POST'){
+      let body = [];
+
+      request.on('data',(chunk) => {
+        body.push(chunk);
+      });
+
+      request.on('end',() => {
+        body = Buffer.concat(body).toString();
+        const {name} = JSON.parse(body);
+        response.end(`<h1> hallo, ${name}! ini adalah halaman about </h1>`);
+      })
+
+    }else{
+      response.end(`<h1>Halaman tidak dapat di akses mengunakan ${method} request !!</h1>`);
+    }
+  }else{
+    response.end('<h1>Halaman tidak ditemukan!!</h1>');
   }
 };
 
